1150833218a3026afe73d24a04528e053780607d
Aadil Saudagar <aadilsaudagar24@gmail.com>
2023-09-29 18:45:21 +0530

    Done with Design Home Page ui

---
+++ ui1/lib/main.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import "package:ui1/home.dart";
+
+void main() {
+  runApp(MaterialApp(
+    title: "My App",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
